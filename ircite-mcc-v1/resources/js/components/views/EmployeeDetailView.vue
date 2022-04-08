2efd6d8da77b58431520ec38b3d6ccb778e5c2c6
Kyle-Mendiola <kyle.l.mendiola@gmail.com>
2022-04-08 01:35:38 +0000

    2nd commit

--- ircite-mcc-v1/resources/js/components/views/EmployeeDetailView.vue
+++ ircite-mcc-v1/resources/js/components/views/EmployeeDetailView.vue
@@ -13,22 +13,35 @@
               <th>Vacation Leave Credits</th>
               <th>Hourly rate</th>
           </tr>
-          <tr>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
+          <tr v-if="employee">
+              <td>{{ employee.firstName }}</td>
+              <td>{{ employee.lastName }}</td>
+              <td>{{ employee.position }}</td>
+              <td>{{ employee.firstName }}</td>
+              <td>{{ employee.sickLeaveCredits }}</td>
+              <td>{{ employee.vacationLeaveCredits }}</td>
+              <td>{{ employee.hourlyRate }}</td>
           </tr>
       </table>
   </div>
 </template>
 
 <script>
-export default {
+import useEmployee from '../../composables/useEmployee'
+import { onMounted } from 'vue'
 
+export default {
+    props: [ "id" ],
+    setup(props) {
+        const { employee , getEmployee } = useEmployee()
+
+        onMounted(() => {
+            getEmployee(props.id)
+            console.log(props.id)
+        })
+
+        return { employee }
+    }
 }
 </script>
 
--- ircite-mcc-v1/resources/js/components/views/EmployeeView.vue
+++ ircite-mcc-v1/resources/js/components/views/EmployeeView.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="employee-details">
       <table>
-          <tr colspan="7"> 
-              <th><h2> Employee Detail </h2> </th>
+          <tr colspan="8"> 
+              <th><h2> Employees List </h2> </th>
           </tr>
           <tr>
               <th>First Name</th>
@@ -13,25 +13,39 @@
               <th>Vacation Leave Credits</th>
               <th>Hourly rate</th>
           </tr>
-          <tr>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
+          <tr v-for="employee in employees" :key="employee.id">
+              <td>{{ employee.firstName }}</td>
+              <td>{{ employee.lastName }}</td>
+              <td>{{ employee.position }}</td>
+              <td>{{ employee.firstName }}</td>
+              <td>{{ employee.sickLeaveCredits }}</td>
+              <td>{{ employee.vacationLeaveCredits }}</td>
+              <td>{{ employee.hourlyRate }}</td>
+              <td> <router-link :to="{ name: 'employee-details', params: { id: employee.id }}"> Details </router-link> </td>
           </tr>
       </table>
   </div>
 </template>
 
 <script>
-export default {
+import useEmployee from '../../composables/useEmployee'
+import { onMounted } from 'vue'
 
+export default {
+    setup() {
+        const { employees , getEmployees } = useEmployee()
+
+        onMounted(() => {
+            getEmployees()
+        })
+
+        return { employees }
+    }
 }
 </script>
 
 <style>
-
+    td, th, h2 {
+        text-align: center;
+    }
 </style>
