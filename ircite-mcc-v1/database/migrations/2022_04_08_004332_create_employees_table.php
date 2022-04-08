fa068677e1a088e4e6fb22841f768ca007b09aeb
dexcabigting <dexcabigting1234@gmail.com>
2022-04-08 00:48:18 +0000

    Created employees table migration file

---
+++ ircite-mcc-v1/database/migrations/2022_04_08_004332_create_employees_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('firstName', 50);
+            $table->string('lastName', 50);
+            $table->string('position', 50);
+            $table->integer('sickLeaveCredits');
+            $table->integer('vacationLeaveCredits');
+            $table->float('hourlyRate', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
