0cf5bb9f507890f43ea778aee1d379c0a90400a3
dexcabigting <dexcabigting1234@gmail.com>
2022-04-08 01:47:56 +0000

    Added id attribute to the json response

--- ircite-mcc-v1/app/Http/Resources/EmployeeResource.php
+++ ircite-mcc-v1/app/Http/Resources/EmployeeResource.php
@@ -16,6 +16,7 @@ class EmployeeResource extends JsonResource
     {
         // return parent::toArray($request);
         return [
+                'id' => $this->id,
                 'firstName' => $this->firstName,
                 'lastName' => $this->lastName,
                 'position' => $this->position,
