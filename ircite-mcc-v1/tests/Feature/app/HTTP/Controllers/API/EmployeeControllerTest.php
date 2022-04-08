1d0c0befa2bfee00925170c0fc37015f23867f4c
dexcabigting <dexcabigting1234@gmail.com>
2022-04-08 02:02:04 +0000

    Added test_if_store_endpoint_responds_201 method

--- ircite-mcc-v1/tests/Feature/app/HTTP/Controllers/API/EmployeeControllerTest.php
+++ ircite-mcc-v1/tests/Feature/app/HTTP/Controllers/API/EmployeeControllerTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature\app\HTTP\Controllers\API;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\Employee;
 
 class EmployeeControllerTest extends TestCase
 {
@@ -15,7 +16,36 @@ class EmployeeControllerTest extends TestCase
      */
     public function test_if_index_endpoint_responds_200()
     {
-        $response = $this->get('/management/employee');
+        $response = $this->get('api/management/employee');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_if_store_endpoint_responds_200()
+    {
+        $data = [
+            'firstName' => 'Pedro',
+            'lastName' => 'Batumbakal',
+            'position' => 'Supervisor',
+            'sickLeaveCredits' => 2,
+            'vacationLeaveCredits' => 2,
+            'hourlyRate' => 1000,
+        ];
+
+        $response = $this->post('api/management/employee', $data);
+
+        $this->assertDatabaseHas('employees', ['firstName' => 'Pedro']);
+
+        $this->assertDatabaseCount('employees', 1);
+
+        $response->assertStatus(201);
+    }
+
+    public function test_if_show_endpoint_responds_200()
+    {
+        $employee = Employee::factory()->create();
+
+        $response = $this->get('api/management/employee' . $employee->id);
 
         $response->assertStatus(200);
     }
